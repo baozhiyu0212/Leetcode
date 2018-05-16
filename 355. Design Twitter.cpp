75e563bd1f1946f8c13a6fed78a78be884edef0d
Baozhi Yu <bayu9537@colorado.edu>
2018-05-15 19:17:18 -0600

    Create 355. Design Twitter.cpp

---
+++ 355. Design Twitter.cpp
@@ -0,0 +1,48 @@
+// Time complexity would be largely depended on how those four operation distribute.
+
+class Twitter {
+    unordered_map<int, unordered_set<int>> m;
+    vector<pair<int,int>> v;
+public:
+    /** Initialize your data structure here. */
+    Twitter() {
+        
+    }
+    
+    /** Compose a new tweet. */
+    void postTweet(int userId, int tweetId) {
+        v.push_back(make_pair(userId,tweetId));
+    }
+    
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    vector<int> getNewsFeed(int userId) {
+        int count = 0;
+        vector<int> res;
+        for(int i=v.size()-1;i>=0&&count<10;i--){
+            if(m[userId].count(v[i].first) || userId==v[i].first) {
+                res.push_back(v[i].second);
+                count++;
+            }
+        }
+        return res;
+    }
+    
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    void follow(int followerId, int followeeId) {
+        m[followerId].insert(followeeId);
+    }
+    
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    void unfollow(int followerId, int followeeId) {
+        m[followerId].erase(followeeId);
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * vector<int> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
